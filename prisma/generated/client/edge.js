98be75a074d59b2f7ca5883495dbc526c4783a52
Ali Jalloul <ali.z.jalloul@gmail.com>
2024-11-20 17:19:34 +0200

    updated cicd 3

--- prisma/generated/client/edge.js
+++ prisma/generated/client/edge.js
@@ -195,7 +195,7 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
-  "postinstall": true,
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
--- prisma/generated/client/index.js
+++ prisma/generated/client/index.js
@@ -196,7 +196,7 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
-  "postinstall": true,
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
